3a80ef4a80aebdf802413bbe315fccd6997f3617
VtheRtech <michaelvcumbo21@proton.me>
2024-10-11 22:16:23 -0400

    added in starship stolen config

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -19,7 +19,7 @@
           # ./programs/kitty.nix
           ./programs/firefox.nix
           ./programs/bash.nix
-         # ./programs/starship.nix
+          ./programs/starship.nix
         ];
         home = {
           inherit username;
