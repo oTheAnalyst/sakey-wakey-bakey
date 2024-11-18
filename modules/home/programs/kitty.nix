f1be4d06b5c3b932f99783f8d8c2bb6dcf9dcb6e
oTheAnalyst <michaelvcumbo21@proton.me>
2024-11-17 19:11:49 -0500

    darked terminal

--- modules/home/programs/kitty.nix
+++ modules/home/programs/kitty.nix
@@ -3,12 +3,12 @@ _: {
     kitty = {
       enable = true;
       extraConfig = ''
-         cremember_window_size  no
-         initial_window_width  640
-         initial_window_height  400
-         confirm_os_window_close  0
-         background_opacity  0.5
-         hide_window_decorations  yes
+        cremember_window_size  no
+        initial_window_width  640
+        initial_window_height  400
+        confirm_os_window_close  0
+        background_opacity  0.8
+        hide_window_decorations  yes
       '';
     };
   };
