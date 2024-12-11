fb50e693ef2c6a5dda0216281ffcbfae0cb7b844
VtheRtech <michaelvcumbo21@proton.me>
2024-12-10 20:41:35 -0500

    updated home-manager from 24.05 to 24.11 and Nixos from 24.05 to 24.11

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     agenix.url = "github:ryantm/agenix";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
