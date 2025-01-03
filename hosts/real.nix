fb86911a30018ed5701c08c9b13dce65a2e45800
oTheAnalyst <michaelvcumbo21@proton.me>
2025-01-03 15:06:14 -0500

    post gres sql db nix setup just following directiob

--- hosts/real.nix
+++ hosts/real.nix
@@ -60,8 +60,18 @@ in {
       enable = true;
       ensureDatabases = ["general"];
       authentication = pkgs.lib.mkOverride 10 ''
-        #type database  DBuser  auth-method
+        #...
+        #type database DBuser origin-address auth-method
         local all       all     trust
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host all       all     ::1/128        trust
+      '';
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+        CREATE DATABASE nixcloud;
+        GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
       '';
     };
     displayManager = {
