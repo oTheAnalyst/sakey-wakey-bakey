249d2f140fa9d463e379febb7cf2c6f43b21b664
oTheAnalyst <michaelvcumbo21@proton.me>
2025-01-07 14:33:55 -0500

    added in lobster

--- hosts/real.nix
+++ hosts/real.nix
@@ -118,7 +118,7 @@ in {
     # custom utility
     (btop.override {rocmSupport = true;})
     (libsForQt5.callPackage futureTeamspeakPackage {})
-    input.lobster.packages.x86_64-linux.lobster
+    lobster.packages.${pkgs.system}.lobster
     # agenix.packages.${pkgs.system}.default
     # utility
     #cli tools
