6e2aaae3f8c1d028bdb03aa2881c498bcf0637ea
oTheAnalyst <michaelvcumbo21@proton.me>
2025-01-07 15:09:22 -0500

    Revert "added in lobster"
    This reverts commit 249d2f140fa9d463e379febb7cf2c6f43b21b664.

--- hosts/real.nix
+++ hosts/real.nix
@@ -118,7 +118,7 @@ in {
     # custom utility
     (btop.override {rocmSupport = true;})
     (libsForQt5.callPackage futureTeamspeakPackage {})
-    lobster.packages.${pkgs.system}.lobster
+    input.lobster.packages.x86_64-linux.lobster
     # agenix.packages.${pkgs.system}.default
     # utility
     #cli tools
