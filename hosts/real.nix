7f0a9ed15f5c52cba615bd6010eef6e139aae2ff
oTheAnalyst <michaelvcumbo21@proton.me>
2025-01-09 07:51:25 -0500

    vanilla postgres db implemented with nixos

--- hosts/real.nix
+++ hosts/real.nix
@@ -57,15 +57,24 @@ in {
     desktopManager.plasma6.enable = true;
     # teamspeak3.enable = true;
     postgresql = {
-      settings = {
-        listen_addresses = "*";
-      };
-      enableTCPIP = true;
       enable = true;
+      ensureDatabases = ["mydatabase"];
+      enableTCPIP = true;
+      # port = 5432;
       authentication = pkgs.lib.mkOverride 10 ''
-        local all       all     trust
-        host all all      ::1/128      trust
-        host all postgres 127.0.0.1/32 trust
+        #type database DBuser origin-address auth-method
+        local all      all     trust
+        # ... other auth rules ...
+
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        # ipv6
+        host  all      all     ::1/128        trust
+      '';
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+        CREATE DATABASE nixcloud;
+        GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
       '';
     };
     displayManager = {
