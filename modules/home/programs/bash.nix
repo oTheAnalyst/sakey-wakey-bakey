9ec2bcc5bd2c205f8892d2e837999f79a6cfc936
oTheAnalyst <michaelvcumbo21@proton.me>
2025-01-09 07:54:38 -0500

    changed bash script for build alias

--- modules/home/programs/bash.nix
+++ modules/home/programs/bash.nix
@@ -13,7 +13,7 @@ _:
     vflake="nix run --no-write-lock-file github:oTheAnalyst/neovim-flake";
     gst = builtins.trace "hello world" "git status";
     gp = "git pull";
-    bfu = "sudo nixos-rebuild switch --flake";
+    buildnix = "sudo nixos-rebuild switch --flake";
     };
    };
   }
