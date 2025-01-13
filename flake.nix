691e6ae58cc25379e229d896a557356c47d10ee9
VtheRtech <michaelvcumbo21@proton.me>
2025-01-13 08:50:40 -0500

    added the shadps4 to gaming machine
    added the shadps4 to gaming machine
    
    added the shadps4 to gaming machine

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     agenix.url = "github:ryantm/agenix";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,24 +11,30 @@
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    shadps4 = {
+      url = "github:shadps4-emu/shadPS4";
+      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+    };
   };
 
   outputs = {
     self,
     agenix,
+    shadps4,
     ...
   } @ inputs: let
     username = "pretender";
   in {
     nixosConfigurations = {
       faker = inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs agenix username;};
+        specialArgs = {inherit inputs agenix username shadps4;};
         system = "x86_64-linux";
         modules = [
           ./modules/home
           ./hosts/faker.nix # host file, hardware, unique stuff
           ./modules/nixos/system.nix # shared system (nixos) module
           agenix.nixosModules.default
+          shadps4.nixosModules.default
         ];
       };
       real = inputs.nixpkgs.lib.nixosSystem {
@@ -37,7 +44,7 @@
           ./modules/home
           ./hosts/real.nix # host file, hardware, unique stuff
           ./modules/nixos/system.nix # shared system (nixos) module
-         # agenix.nixosModules.default
+          # agenix.nixosModules.default
         ];
       };
     };
