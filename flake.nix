9208e132d439bcd70c224932e09fd38b53a0dd7d
VtheRtech <michaelvcumbo21@proton.me>
2025-01-13 08:58:26 -0500

    added the shadps4 to gaming machine

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     agenix.url = "github:ryantm/agenix";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
