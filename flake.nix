ab5087aae5ba0538492909179c42b0cb5d1a7352
VtheRtech <michaelvcumbo21@proton.me>
2025-01-13 11:04:04 -0500

    fixed shad ps4

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     agenix.url = "github:ryantm/agenix";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,13 +22,15 @@
   in {
     nixosConfigurations = {
       faker = inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs agenix username ;};
+        specialArgs = {inherit inputs agenix username;};
         system = "x86_64-linux";
         modules = [
           ./modules/home
           ./hosts/faker.nix # host file, hardware, unique stuff
           ./modules/nixos/system.nix # shared system (nixos) module
           agenix.nixosModules.default
+     ({ pkgs, ... }: {
+        environment.systemPackages = [ inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.shadps4 ]; })
         ];
       };
       real = inputs.nixpkgs.lib.nixosSystem {
