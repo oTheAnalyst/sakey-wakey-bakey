adfdcaab812b06da84a3c47690b72bb18efe94ff
VtheRtech <michaelvcumbo21@proton.me>
2025-01-21 21:26:48 -0500

    fix for latex

--- hosts/faker.nix
+++ hosts/faker.nix
@@ -111,7 +111,7 @@ in {
     # utility
     lyx
     texliveFull # another latex thing
-    textliveTeTeX
+    texliveTeTeX
     texlivePackages.enumitem #for latex to work 
     protonvpn-cli_2
     bitwarden-cli
