6a2707161807f01f2ec4d1938bcc3410998dc85a
oTheAnalyst <michaelvcumbo21@proton.me>
2025-03-04 19:39:53 -0500

    fixed some of the structure of code

--- hosts/underblade.nix
+++ hosts/underblade.nix
@@ -6,12 +6,7 @@
   modulesPath,
   username,
   ...
-}: let
-  futureTeamspeakPackage = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/NixOS/nixpkgs/fa3a54cd07014a17df3e24946c5138d5501a416c/pkgs/applications/networking/instant-messengers/teamspeak/client.nix";
-    sha256 = "1jq0xgwr6lwnm57bgdynv2xm6ma6wp6rriim2lzb6xr6207pckd9";
-  };
-in {
+}: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   #age.secrets.secret1.file = ../secret1.age;
@@ -128,7 +123,6 @@ in {
   environment.systemPackages = with pkgs; [
     # custom utility
     (btop.override {rocmSupport = true;})
-    (libsForQt5.callPackage futureTeamspeakPackage {})
     # agenix.packages.${pkgs.system}.default
     # utility
     #cli tools
@@ -183,7 +177,6 @@ in {
     pgadmin4
     qpwgraph
     teams-for-linux
-    teamspeak3
     qbittorrent
     bitwarden-desktop
     # hyprland apps
