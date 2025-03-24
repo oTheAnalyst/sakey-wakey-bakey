3a6e03203d07e4802d3c2b81f94c2602498f27ab
oTheAnalyst <michaelvcumbo21@proton.me>
2025-03-24 00:28:14 -0400

    updated agenix secrets

--- hosts/underblade.nix
+++ hosts/underblade.nix
@@ -12,7 +12,7 @@
 
   age.secrets = {
     secret1.file = ../secret1.age;
-    secret2.file = ../s2.age;
+    s2.file = ../s2.age;
   };
 
   age.identityPaths = ["/home/pretender/.ssh/ed123" "/home/pretender/.ssh/id_ed25519"];
