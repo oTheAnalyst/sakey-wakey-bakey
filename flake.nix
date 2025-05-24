f38da133277eb40b1838476bd36a70350753256d
VtheRtech <michaelvcumbo21@proton.me>
2025-05-24 16:27:32 -0400

    updated flake to 25.06

--- flake.nix
+++ flake.nix
@@ -1,13 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     agenix.url = "github:ryantm/agenix";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-25.05";
     };
     hyprland = {
       url = "github:hyprwm/Hyprland";
