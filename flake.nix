21780e92e91db48eaa9ec7b54f890bc02bfd3167
oTheAnalyst <michaelvcumbo21@proton.me>
2025-07-07 10:47:43 -0400

    move home-manager from 25.11 to master

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/master";
     };
     hyprland = {
       url = "github:hyprwm/Hyprland";
