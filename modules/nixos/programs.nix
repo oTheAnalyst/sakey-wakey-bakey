2e49d7021f451c06d47512a5149f2f680148a086
oTheAnalyst <michaelvcumbo21@proton.me>
2025-08-18 10:56:01 -0400

    removed agenix added another module for programs

---
+++ modules/nixos/programs.nix
@@ -0,0 +1,35 @@
+{...}:
+{
+
+  programs = {
+    firefox.enable = true;
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+    };
+    openvpn3.enable = true;
+    hyprland.enable = true;
+    virt-manager.enable = true;
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = false;
+        nix_shell = {
+          symbol = " ";
+          format = "$symbol ";
+        };
+        hostname.format = "$hostname:";
+        username.format = "$user@";
+      };
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode.enable = true;
+  };
+
+
+
+     }
