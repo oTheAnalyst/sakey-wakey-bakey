2d3f58d364f777400a5fad2bdde03ee8b14058ce
VtheRtech <michaelvcumbo21@proton.me>
2025-09-06 00:32:47 -0400

    added walker

--- modules/home/programs/walker.nix
+++ modules/home/programs/walker.nix
@@ -1,14 +1,15 @@
 {
-  programs.walker = {
+  services.walker = {
     enable = true;
-    runAsService = true;
 
     # All options from the config.json can be used here.
-    config = {
+    settings = {
       search.placeholder = "Example";
       ui.fullscreen = true;
       list = {
         height = 200;
+        width = 600;
+        image_size = 65;
       };
       websearch.prefix = "?";
       switcher.prefix = "/";
@@ -16,9 +17,38 @@
 
     # If this is not set the default styling is used.
     theme.style = ''
-      * {
-        color: #dcd7ba;
-      }
-    '';
+      *  {
+       border-radius: 3px;
+       border: none;
+           }
+
+           window {
+           font-size: 32px;
+           font-family: "Roboto Mono Medium";
+           background-color: rgba(50, 50, 50, 0.9);
+           color: white;
+           border-bottom: 3px
+           }
+
+           #entry{
+               paddin: 0.25em;
+           }
+
+
+           #entry:selected {
+               background-color: #bbccdd;
+               background: linear-gradient(#90deg, #bbffdd, #dd77ff);
+           }
+
+           #text:selected {
+               color: #333333;
+           }
+
+           #input {
+               background-color: rgba(50, 50, 50, 0.5);
+               color: white;
+               padding: 0.25em;
+
+           } '';
   };
 }
