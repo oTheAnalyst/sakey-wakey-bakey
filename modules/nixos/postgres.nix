b3e415a498eff1da62e6785fe1ad725deef752b0
VtheRtech <michaelvcumbo21@proton.me>
2025-09-17 09:35:46 -0400

    finally was able to connect to the local cluster with this fix

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -1,11 +1,16 @@
-{pkgs, ...}:
-{
-  config.services.postgresql = {
+pkgs: {
+  # ...
+  services.postgresql = {
     enable = true;
-    ensureDatabases = [ "mydatabase" ];
+    ensureDatabases = ["mydatabase"];
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
+      # type database  DBuser  auth-method
       local all       all     trust
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      host  all      postgres   0.0.0.0/0  trust
+      # ipv6
+      host  all      all     ::1/128        trust
     '';
   };
 }
