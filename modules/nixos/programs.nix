f6f42c95c7d525e9022f652236d7551259b91bc5
oTheAnalyst <michaelvcumbo21@proton.me>
2025-09-27 22:59:28 -0400

    added hyprland to homemanager

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -7,7 +7,6 @@
       clean.extraArgs = "--keep-since 4d --keep 3";
     };
     openvpn3.enable = false;
-    hyprland.enable = true;
     virt-manager.enable = true;
     waybar.enable = true;
     starship = {
