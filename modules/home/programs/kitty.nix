a7a00ad2f62dde787c431e30e187e96ff03b9096
oTheAnalyst <michaelvcumbo21@proton.me>
2025-10-19 13:41:05 -0400

    fixed color of terminals

--- modules/home/programs/kitty.nix
+++ modules/home/programs/kitty.nix
@@ -4,81 +4,99 @@
       enable = true;
       font.name = "nerd-fonts-fira-code";
       extraConfig = ''
-        cremember_window_size  no
-        initial_window_width  640
-        initial_window_height  400
-        confirm_os_window_close  0
-        background_opacity  0.8
-        hide_window_decorations  yes
+                cremember_window_size  no
+                initial_window_width  640
+                initial_window_height  400
+                confirm_os_window_close  0
+                background_opacity  0.95
+                hide_window_decorations  yes
 
-          # Basic colors
-          foreground           #d8dee9
-          background           #2e3440
-          selection_foreground #d8dee9
-          selection_background #434c5e
+         #: The basic colors
 
-          # Cursor colors
-          cursor            #d8dee9
-          cursor_text_color #3b4252
+        foreground #878ba6
+        background #1e202f
+        selection_foreground #878ba6
+        selection_background #292c3d
 
-          # URL underline color when hovering with mouse
-          url_color #0087bd
 
-          # Window border colors and terminal bell colors
-          active_border_color   #81a1c1
-          inactive_border_color #4c566a
-          bell_border_color     #88c0d0
-          visual_bell_color     none
+        #: Cursor colors
 
-          # Tab bar colors
-          active_tab_foreground   #3b4252
-          active_tab_background   #88c0d0
-          inactive_tab_foreground #e5e9f0
-          inactive_tab_background #4c566a
-          tab_bar_background      #3b4252
-          tab_bar_margin_color    none
+        cursor #d14781
+        cursor_text_color #1e202f
 
-          # Mark colors (marked text in the terminal)
-          mark1_foreground #3b4252
-          mark1_background #88c0d0
-          mark2_foreground #3b4252
-          mark2_background #bf616a
-          mark3_foreground #3b4252
-          mark3_background #ebcb8b
 
-          # The basic 16 colors
-          # black
-          color0 #3b4252
-          color8 #4c566a
+        #: URL underline color when hovering with mouse
 
-          # red
-          color1 #bf616a
-          color9 #bf616a
+        url_color #d2d8fe
 
-          # green
-          color2  #a3be8c
-          color10 #a3be8c
 
-          # yellow
-          color3  #ebcb8b
-          color11 #d08770
+        #: kitty window border colors and terminal bell colors
 
-          # blue
-          color4  #81a1c1
-          color12 #5e81ac
+        active_border_color #444864
+        inactive_border_color #1e202f
+        bell_border_color #5165e6
+        visual_bell_color none
 
-          # magenta
-          color5  #b48ead
-          color13 #b48ead
 
-          # cyan
-          color6  #88c0d0
-          color14 #8fbcbb
+        #: OS Window titlebar colors
 
-          # white
-          color7  #e5e9f0
-          color15 #eceff4
-      '';
+        wayland_titlebar_color #292c3d
+        macos_titlebar_color #292c3d
+
+
+        #: Tab bar colors
+
+        active_tab_foreground #fbf9fa
+        active_tab_background #1e202f
+        inactive_tab_foreground #b0a6aa
+        inactive_tab_background #292c3d
+        tab_bar_background #292c3d
+        tab_bar_margin_color none
+
+
+        #: Colors for marks (marked text in the terminal)
+
+        mark1_foreground #1e202f
+        mark1_background #6375ee
+        mark2_foreground #1e202f
+        mark2_background #8d8186
+        mark3_foreground #1e202f
+        mark3_background #e44e8c
+
+
+        #: The basic 16 colors
+
+        #: black
+        color0 #1e202f
+        color8 #4f5472
+
+        #: red
+        color1 #7586f5
+        color9 #fe81b5
+
+        #: green
+        color2 #fb6fa9
+        color10 #292c3d
+
+        #: yellow
+        color3 #ffb3d2
+        color11 #444864
+
+        #: blue
+        color4 #8696fd
+        color12 #5b6080
+
+        #: magenta
+        color5 #fb6fa9
+        color13 #d2d8fe
+
+        #: cyan
+        color6 #a0acfe
+        color14 #f764a1
+
+        #: white
+        color7 #878ba6
+        color15 #ebedff     '';
     };
   };
 }
