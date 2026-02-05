9da48569b752c7dea98bc732db0559587318c82f
VtheRtech <michaelvcumbo21@proton.me>
2026-02-04 20:35:22 -0500

    update

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -3,7 +3,7 @@
 hardware.keyboard.zsa.enable = true;
 nixpkgs.config.permittedInsecurePackages = [
                 "qtwebengine-5.15.19"
-                  "ventoy-1.1.07"
+                  "ventoy-1.1.10"
               ];
 services = {
     cron = {
