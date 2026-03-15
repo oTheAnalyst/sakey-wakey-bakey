5c1a794d525ea89383d30ab85758957767a4fa16
oTheAnalyst <michaelvcumbo21@proton.me>
2026-03-15 13:27:42 -0400

    added hypridle removed ventoy setup printer

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs,username, ...}: {
   services = {
     avahi = {
       enable = true;
@@ -21,6 +21,17 @@
       #   ];
     };
   };
+  environment.systemPackages = with pkgs; [
+    ddcutil
+  ];
+
+  services.udev.extraRules = ''
+    KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
+  '';
+  boot.kernelModules = ["i2c-dev"];
+  users.groups.i2c = {};
+  users.users.${username}.extraGroups = ["i2c"];
+
   hardware.keyboard.zsa.enable = true;
   programs = {
     firefox.enable = true;
