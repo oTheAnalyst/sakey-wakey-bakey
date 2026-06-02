cb20c11ee2556d3ce45bc8806b1e25b0822eb224
oTheAnalyst <michaelvcumbo21@proton.me>
2026-06-02 14:50:56 -0400

    added nvf and configured it, made fish the default shell used "programs" added closing brackets plugs, made fish the default shell.

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -55,12 +55,20 @@
 
   hardware.keyboard.zsa.enable = true;
   programs = {
-    firefox.enable = true;
-    hyprland.enable = true;
-    nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 4d --keep 3";
+    bash = {
+          interactiveShellInit = ''
+            # "check if parent process is not fish" && "make nested shells work properly"
+            if grep -qv fish /proc/$PPID/comm && [[ $SHLVL == [12] ]]; then
+                # set $SHELL for better integration with programs like nix shell, tmux, etc.
+                SHELL=${pkgs.fish}/bin/fish exec fish
+            fi '';
+                        };
+            firefox.enable = true;
+            hyprland.enable = true;
+            nh = {
+              enable = true;
+              clean.enable = true;
+              clean.extraArgs = "--keep-since 4d --keep 3";
     };
     git = {
       config = [
