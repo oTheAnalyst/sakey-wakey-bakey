60a6286b28c353a8692e52e631dc8ffcece49637
oTheAnalyst <michaelvcumbo21@proton.me>
2026-08-02 13:14:18 -0400

    updated shadps4 and system, removed some large unused tools, added lazy git

--- modules/nixos/shadps4Practice.nix
+++ modules/nixos/shadps4Practice.nix
@@ -5,16 +5,19 @@
   unzip,
   ...
 }: let
-  pname = "shadps4";
-  version = "224";
+  pname = "shadPS4QtLauncher";
+  rev = "88b34250839113dfdab1d3b8f1a7bfa559ea95e4";
+  version = "2026-08-07-88b3425";
 
-  zipSrc = fetchurl {
-    url = "https://github.com/shadps4-emu/shadps4-qtlauncher/releases/download/v${version}/shadPS4QtLauncher-linux-qt-v${version}.zip";
-    hash = "sha256-CMPG2mSizqHURwE5q5OI3iQoKYBu2HoI6mOTHgExyxc=";
-  };
+  zipSrc =
+    fetchurl {
+      url = "https://github.com/shadps4-emu/shadps4-qtlauncher/releases/download/shadPS4QtLauncher-2026-08-07-${rev}/shadPS4QtLauncher-linux-qt-${version}.zip";
+      hash = "sha256-hbLWhTbQN0fSIWo/Ykxs+AQnWmNvyCbDNzrFFt8evUQ=
+";
+    };
 
   appimageSrc =
-    runCommand "${pname}-${version}-extracted.AppImage"
+    runCommand "${pname}-qt.AppImage"
     {
       nativeBuildInputs = [unzip];
     }
